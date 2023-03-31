4829d5e64150ddf43ff2d6dfb3e3889dee45ad2a
faeefa <faeefa5@gmail.com>
2023-03-31 21:08:25 +0300

    all

---
+++ 0x04-TypeScript/task_0/main.ts
@@ -0,0 +1,10 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1:Student = {firstName: "tsion" , lastName: "eleke", age: 27 ,location: "addis ababa"};
+const student2:Student = {firstName: "robel", lastName: "eleke", age: 17, location: "addis ababa"}
+const sttudentsList = [student1, student2];
